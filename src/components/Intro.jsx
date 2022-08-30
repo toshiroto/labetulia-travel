0abe4c42cfa0aa355205196d8a895e836a914365
toshiroto <chtopete@gmail.com>
2022-08-30 19:04:22 +0900

    App set up and rendered

---
+++ src/components/Intro.jsx
@@ -0,0 +1,7 @@
+function Intro () {
+  return (
+    <h1>La Betulia Travel</h1>
+  )
+}
+
+export default Intro;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+function Navbar () {
+  return (
+    <h1>Navbar Goes Here</h1>
+  )
+}
+
+export default Navbar;
